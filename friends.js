de96766f1476493fad5ae409d109f54f7c7223be
Judy Ye <judyy.ye@gmail.com>
2023-04-20 18:57:36 -0700

    add another for statement

---
+++ friends.js
@@ -0,0 +1,14 @@
+var numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+
+// for (i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > 30) {
+//         console.log(numbers[i]);
+//     }
+    
+// }
+
+for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 60) {
+        console.log(numbers[i])
+    }
+}
